9f83c51918d3106d9e977de0b40bc7641f22a11e
Dragoslav Petrovic <dragoslav.petrovic@halcom.rs>
2022-10-18 19:29:14 +0200

    Command pattern example with text editor

---
+++ behavioral/command/src/main/java/org/mxhc/CommandDemo.java
@@ -0,0 +1,10 @@
+package org.mxhc;
+
+import org.mxhc.editor.Editor;
+
+public class CommandDemo {
+    public static void main(String[] args) {
+        Editor editor = new Editor();
+        editor.init();
+    }
+}
